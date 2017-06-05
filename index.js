0acaf7f9ddb3dbdbbfb0a949e575aa7839e10ffe
yuxin <yuxin@guazi.com>
2017-06-05 10:47:19 +0800

    gitlab http

--- index.js
+++ index.js
@@ -6,12 +6,21 @@ const contentstream = require('contentstream')
 const exec = require('child_process').exec
 const Q = require('q')
 const pts = require('promise-from-stream')
-
+/* params
+ * repo String
+ * options Object
+ *   clone Boolean
+ *   protocol String
+ */
 module.exports = async(repo, options) => {
     repo = normalize(repo)
-    let url = getUrl(repo, options.clone)
+    let url = getUrl(repo, options)
     options = Object.assign({
         dest: options.clone ? options.name || repo.name : '.',
+    }, {
+        clone: false,
+        protocol: 'https',
+        ssh: true
     }, options)
 
     if (options.clone) {
@@ -30,20 +39,21 @@ module.exports = async(repo, options) => {
     }
 }
 
-function getUrl(repo, clone) {
+function getUrl(repo, options) {
     let url
-    if (clone) {
-        url = "git@" + repo.host + ":" + repo.owner + "/" + repo.name + ".git"
+    if (options.clone) {
+        let p = options.ssh ? 'git@' : options.protocol + '://'
+        url = p + repo.host + ':' + repo.owner + '/' + repo.name + '.git'
     } else {
         switch (repo.type) {
             case 'github':
-                url = "https://" + repo.host + "/" + repo.owner + "/" + repo.name + "/archive/" + repo.checkout + ".zip"
+                url = options.protocol + '://' + repo.host + '/' + repo.owner + '/' + repo.name + '/archive/' + repo.checkout + '.zip'
                 break
             case 'gitlab':
-                url = "https://" + repo.host + "/" + repo.owner + "/" + repo.name + "/repository/archive.zip?ref=" + repo.checkout
+                url = options.protocol + '://' + repo.host + '/' + repo.owner + '/' + repo.name + '/repository/archive.zip?ref=' + repo.checkout
                 break
             case 'bitbucket':
-                url = "https://" + repo.host + "/" + repo.owner + "/" + repo.name + "/get/" + repo.checkout + ".zip"
+                url = options.protocol + '://' + repo.host + '/' + repo.owner + '/' + repo.name + '/get/' + repo.checkout + '.zip'
                 break
             default:
 
